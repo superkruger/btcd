98642cff5690da3d427cd17e17ee86813b283eb3
superkruger <superkruger@gmail.com>
2022-03-09 12:26:14 +0100

    working version that can expand to parallel workers, and can be stopped by the client

--- wire/delegatemessages.go
+++ wire/delegatemessages.go
@@ -11,5 +11,14 @@ type HeaderProblemResponse struct {
 type HeaderProblemRequest struct {
 	Address         string
 	BlockHeight     int32
-	HashesPerSecond int32
+	HashesCompleted uint64
+}
+
+type HeaderSolution struct {
+	Type        string
+	Address     string
+	Nonce       uint32
+	ExtraNonce  uint64
+	BlockHeight int32
+	BlockHash   string
 }
