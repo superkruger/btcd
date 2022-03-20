36e8fe9d13d4e295d31a42c6955aabe1b1199dea
superkruger <superkruger@gmail.com>
2022-03-20 14:30:36 +0100

    fixed some issues with uint64 javascript compatibility and error handling

---
+++ database/delegate/solution.go
@@ -0,0 +1,56 @@
+package delegate
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"sync"
+	"time"
+)
+
+type Hashrates struct {
+	mu sync.Mutex
+	db *sql.DB
+}
+
+type Hashrate struct {
+	Address    string
+	MeasuredAt time.Time
+	Hashrate   uint64
+}
+
+const file string = "hashrates.db"
+
+const create string = `
+  CREATE TABLE IF NOT EXISTS hashrates (
+  	address TEXT NOT NULL,
+    measured_at DATETIME NOT NULL,
+    hashrate INTEGER NOT NULL
+  );
+  CREATE INDEX IF NOT EXISTS idx_address_date
+    ON hashrates (address, measured_at);`
+
+func NewHashrates() (*Hashrates, error) {
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := db.Exec(create); err != nil {
+		return nil, err
+	}
+	return &Hashrates{
+		db: db,
+	}, nil
+}
+
+func (c *Hashrates) Insert(hashrate Hashrate) error {
+	_, err := c.db.Exec("INSERT INTO hashrates VALUES(?,?,?);", hashrate.Address, hashrate.MeasuredAt, hashrate.Hashrate)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Hashrates) Close() error {
+	return c.db.Close()
+}
